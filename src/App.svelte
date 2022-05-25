db2d1938b8015d3c947272a90e80da50bad81aaf
Kurt Snuggly <unsnugHero@gmail.com>
2022-05-24 22:22:29 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<main>
+	<h1 class="title">
+		<span class="title-first">Poké</span><span class="title-second">Height</span>
+	</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.title-first {
+		font-family: poke-hollow;
+	}
+
+	.title-second {
+		font-family: poke-solid;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+</style>
