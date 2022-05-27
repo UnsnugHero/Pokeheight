bedcd0bf18ded3159af284683c3031b4830b08f1
Kurt Snuggly <unsnugHero@gmail.com>
2022-05-26 20:37:54 -0400

    start button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let buttonText = 'Button';
+
+	export let onClickHandler = function() {
+		console.warn('Unimplemented button handler!!!')
+	};
+</script>
+
+<div class="button-wrapper">
+	<button on:click={onClickHandler}>{buttonText}</button>
+</div>
+
+<style>
+
+</style>
--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,3 +1,11 @@
-<div class="form-container">
+<script>
+	import Button from './Button.svelte';
 
+	const submitFormHandler = function() {
+		console.log("SUBMIT!");
+	}
+</script>
+
+<div class="form-container">
+	<Button buttonText='Go!' onClickHandler={submitFormHandler} />
 </div>
