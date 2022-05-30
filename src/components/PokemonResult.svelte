fc3fbb11992feb40f78fdd937a40db57f64896d1
Kurt Snuggly <unsnugHero@gmail.com>
2022-05-30 00:36:25 -0400

    render num pokemon in height

--- src/components/PokemonResult.svelte
+++ src/components/PokemonResult.svelte
@@ -3,24 +3,39 @@
 	export let pokemon;
 
 	const getPokemonFitToTrainerHeight = function(pkmnHeight, trainerHeight) {
-		const numPokemon = trainerHeight / pkmnHeight;
-		console.log(numPokemon);
-		const imgArray = [Math.round(numPokemon)].map(n => pokemon.sprite);
-		const pokeLeft = numPokemon % 1;
+		let numPokemon = trainerHeight / pkmnHeight;
+		const imgArray = [];
+
+		while(numPokemon > 1) {
+			imgArray.push(pokemon.sprite);
+			numPokemon -= 1;
+		}
+		const pokeLeft = numPokemon;
 
 		return { imgArray, pokeLeft }
 	}
 
-	const { sprite, name, height } = pokemon;
-	const pokeFitToTrainerHeight = getPokemonFitToTrainerHeight(height, trainerHeight);
-
-	console.log(pokeFitToTrainerHeight);
+	$: pokeFitToTrainerHeight = getPokemonFitToTrainerHeight(pokemon.height, trainerHeight);
 
 </script>
 
 <div class="pokemon-result-container">
-	<p>{pokemon.name}</p>
-	<p>{pokemon.height}</p>
-	<img src={pokemon.sprite} alt={pokemon.name}/>
-	<p>Trainer height: {trainerHeight}</p>
+	<div class="pokemon-stacked">
+		{#each pokeFitToTrainerHeight.imgArray as sprite}
+			<img src={sprite} alt="poke-height" />
+		{/each}
+	</div>
+	<span>Your height in {pokemon.name}'s</span>
 </div>
+
+<style>
+	.pokemon-result-container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.pokemon-stacked {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
