b4e82a11ce683bce51d1cc8ec29c1b382421fb9f
Kurt Snuggly <unsnugHero@gmail.com>
2022-05-29 23:37:01 -0400

    begin processing trainer height to pokemon height

--- src/components/PokemonResult.svelte
+++ src/components/PokemonResult.svelte
@@ -1,6 +1,21 @@
 <script>
 	export let trainerHeight;
 	export let pokemon;
+
+	const getPokemonFitToTrainerHeight = function(pkmnHeight, trainerHeight) {
+		const numPokemon = trainerHeight / pkmnHeight;
+		console.log(numPokemon);
+		const imgArray = [Math.round(numPokemon)].map(n => pokemon.sprite);
+		const pokeLeft = numPokemon % 1;
+
+		return { imgArray, pokeLeft }
+	}
+
+	const { sprite, name, height } = pokemon;
+	const pokeFitToTrainerHeight = getPokemonFitToTrainerHeight(height, trainerHeight);
+
+	console.log(pokeFitToTrainerHeight);
+
 </script>
 
 <div class="pokemon-result-container">
